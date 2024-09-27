37141de9e91b7b0c1d25aa25e730cba9c8eb7acb
Víctor Pérez Figueras <vperez-f@student.42barcelona.com>
2024-09-27 18:38:12 +0200

    ++ check glwf etc...

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: vperez-f <vperez-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 13:48:15 by vperez-f          #+#    #+#             */
-/*   Updated: 2024/09/27 15:29:11 by vperez-f         ###   ########.fr       */
+/*   Updated: 2024/09/27 18:30:24 by vperez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,27 @@
 # include <limits.h>
 # include "../libft/libft.h"
 # include "../printf/ft_printf.h"
-#include  "../mlx/MLX42/include/MLX42/MLX42.h"
+# include "../mlx/MLX42/include/MLX42/MLX42.h"
+
+typedef struct s_scene
+{
+	mlx_t			*mlx;
+	mlx_image_t		*image;
+	int				height;
+	int				width;
+
+}			t_scene;
+
+typedef struct s_camera
+{
+	
+}			t_camera;
+
+typedef struct s_vect
+{
+	float	x;
+	float	y;
+	float	z;
+}			t_vect;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,16 +6,18 @@
 /*   By: vperez-f <vperez-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 13:48:26 by vperez-f          #+#    #+#             */
-/*   Updated: 2024/09/27 15:29:38 by vperez-f         ###   ########.fr       */
+/*   Updated: 2024/09/27 18:33:54 by vperez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/miniRT.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
-    printf("holaaaaa\n");
-    return (0);
+	(void)argc;
+	(void)argv;
+	t_scene	scene;
+
+	scene.mlx = mlx_init(scene.width, scene.height, "miniRT", true);
+	return (0);
 }
