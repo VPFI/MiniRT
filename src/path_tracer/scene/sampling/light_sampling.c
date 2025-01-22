a05097769be33e541eee57ad6171c5abbe137063
Víctor Pérez Figueras <vperez-f@student.42barcelona.com>
2025-01-22 17:00:00 +0100

    --

--- src/path_tracer/scene/sampling/light_sampling.c
+++ src/path_tracer/scene/sampling/light_sampling.c
@@ -6,7 +6,7 @@
 /*   By: vperez-f <vperez-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 14:36:21 by vperez-f          #+#    #+#             */
-/*   Updated: 2025/01/20 14:38:40 by vperez-f         ###   ########.fr       */
+/*   Updated: 2025/01/22 16:59:47 by vperez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,38 +37,47 @@ static t_vect	sample_area(uint32_t *state, t_object *light)
 	return (vect_add(light->get_origin(light), vect_simple_mult(get_random_uvect(state), light->figure.p_light.radius_shadow)));
 }
 
-t_color	light_sampling(t_thread *thread, t_hit_info hit_info, t_mat_type scatter_type)
+static void	calc_material_emittance(t_thread *thread, t_hit_info hit_info, t_object *obj, t_mat_type scatter_type, t_vect *emittance)
 {
-	t_ray		shadow_ray;
 	t_hit_info	test_hit;
-	float		mod;
-	float		mod2;
-	t_object	*temp;
-	t_color 	emittance;
-
+	t_ray		shadow_ray;
+	float		visual_angle;
+	float		distance_decay;
 	t_vect		bound_to_light;
 
-	emittance = new_color(0.0, 0.0, 0.0);
-	temp = thread->scene->lights;
-	while (temp)
+	shadow_ray.origin = hit_info.point;
+	shadow_ray.dir = vect_subtract(sample_area(thread->state, obj), hit_info.point);
+	bound_to_light = vect_subtract(obj->figure.p_light.location, hit_info.point);
+	if (!shadow_hit(thread->scene->objects, shadow_ray, &test_hit, vect_length(bound_to_light)))
 	{
-		shadow_ray.origin = hit_info.point;
-		bound_to_light = vect_subtract(temp->figure.p_light.location, hit_info.point);
-		shadow_ray.dir = vect_subtract(sample_area(thread->state, temp), hit_info.point);
-		if (!shadow_hit(thread->scene->objects, shadow_ray, &test_hit, vect_length(bound_to_light)))
+		distance_decay = 1 / vect_length(bound_to_light);
+		if (scatter_type == LAMBERTIAN)
 		{
-			mod2 = 1 / vect_length(bound_to_light);
-			if (scatter_type == LAMBERTIAN)
-			{
-				mod = vect_dot(hit_info.normal, unit_vect(bound_to_light));
-				if (mod < 0)
-					mod = 0;
-				emittance = vect_add(emittance, vect_simple_mult(temp->material.color, (temp->material.emission_intensity * mod * mod2)));
-			}
-			else if (scatter_type == METAL)
-				emittance = vect_add(emittance, vect_simple_mult(temp->material.color, (p_light_specular(hit_info, shadow_ray, thread->scene->camera.orientation) * temp->material.emission_intensity) * mod2 * 10));
+			visual_angle = vect_dot(hit_info.normal, unit_vect(bound_to_light));
+			if (visual_angle < 0)
+				visual_angle = 0;
+			(*emittance) = vect_add((*emittance), vect_simple_mult(obj->material.color,
+				(obj->material.emission_intensity * visual_angle * distance_decay)));
 		}
-		temp = temp->next;
+		else if (scatter_type == METAL)
+			(*emittance) = vect_add((*emittance), vect_simple_mult(obj->material.color,
+				(p_light_specular(hit_info, shadow_ray, thread->scene->camera.orientation)
+				* obj->material.emission_intensity) * distance_decay * 10));
 	}
-	return (emittance);	
+}
+
+t_color	light_sampling(t_thread *thread, t_hit_info hit_info, t_mat_type scatter_type)
+{
+	t_object	*obj;
+	t_color 	emittance;
+
+	emittance = new_color(0.0, 0.0, 0.0);
+	obj = thread->scene->lights;
+	while (obj)
+	{
+		hit_info.object = obj;
+		calc_material_emittance();
+		obj = obj->next;
+	}
+	return (emittance);
 }
